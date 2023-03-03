4ebd1d3438bfb5ab7b27cd17fa1b1711de61c598
Aleksej_Shishov <aleksejsisov43464@gmail.com>
2023-03-03 06:48:31 +0300

    Add project files.

---
+++ Task_0/Program.cs
@@ -0,0 +1,8 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int result = 50 * 30;
+        Console.WriteLine($"Маша, Вы выглядите на  {result} лет");
+    }
+}
